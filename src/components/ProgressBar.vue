809b4a3536bbb9c415f8f8dd29c1b81bd25a4d94
nmouhammad <39739945+nmouhammad@users.noreply.github.com>
2024-02-29 11:41:48 +0100

    Fill ProgressBar with content
    Each ProgressBar-Element now has an abbreviation that is shown and they are created from an array.

--- src/components/ProgressBar.vue
+++ src/components/ProgressBar.vue
@@ -1,3 +1,17 @@
+<script setup>
+  import { ref } from 'vue'
+
+  const steps = ref([
+    { name: 'Audience-Beginning', abbreviation: 'AB' },
+    { name: 'Introduction', abbreviation: 'I' },
+    { name: 'Main-1', abbreviation: '1' },
+    { name: 'Main-2', abbreviation: '2' },
+    { name: 'Main-3', abbreviation: '3' },
+    { name: 'End', abbreviation: 'E' },
+    { name: 'Audience-End', abbreviation: 'AE' }
+  ])
+</script>
+
 <script>
   import ProgressBarCircle from './ProgressBarCircle.vue'
 
@@ -10,8 +24,12 @@
 </script>
 
 <template>
-  <div>
-    <ProgressBarCircle />
+  <div class="container">
+    <div class="row align-items-center">
+      <div v-for="step in steps" :key="step.id" class="mt-1 col-md-1">
+        <ProgressBarCircle :key="step.id" :abbreviation="step.abbreviation" />
+      </div>
+    </div>
   </div>
 </template>
 
--- src/components/ProgressBarCircle.vue
+++ src/components/ProgressBarCircle.vue
@@ -2,12 +2,19 @@
   // @vuese
   // One of the circles representing one step in the ProgressBar.
   export default {
-    name: 'ProgressBarCircle'
+    name: 'ProgressBarCircle',
+    props: {
+      // Abbreviation of the step title (should be <=2 letters) that will be displayed inside the circle
+      abbreviation: {
+        type: String,
+        required: true
+      }
+    }
   }
 </script>
 
 <template>
-  <div><p>ProgressBarCircle</p></div>
+  <button class="btn btn-primary">{{ abbreviation }}</button>
 </template>
 
 <style scoped></style>
