62d7e991277f13e5c31c5f9f8ed8773902c5cbb4
nmouhammad <39739945+nmouhammad@users.noreply.github.com>
2024-02-29 09:31:20 +0100

    Create ProgressBar class

---
+++ src/components/ProgressBar.vue
@@ -0,0 +1,13 @@
+<script>
+  // @vuese
+  // The progress indicator on top showing users which step of the message composition they are in.
+  export default {
+    name: 'ProgressBar'
+  }
+</script>
+
+<template>
+  <div><p>Content of ProgressBar</p></div>
+</template>
+
+<style scoped></style>
---
+++ vuese-documentation/components/ProgressBar.md
@@ -0,0 +1,4 @@
+# ProgressBar
+
+The progress indicator on top showing users which step of the message composition they are in.
+
