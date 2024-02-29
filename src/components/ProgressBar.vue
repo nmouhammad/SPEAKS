222812e2052d3ec1de0c31620c394e584ab72050
nmouhammad <39739945+nmouhammad@users.noreply.github.com>
2024-02-29 11:47:19 +0100

    Move steps array to ProgressStore
    The steps array will probably need to be accessed by multiple components, therefore it is easier to put it into a store.

--- src/components/ProgressBar.vue
+++ src/components/ProgressBar.vue
@@ -1,15 +1,8 @@
 <script setup>
-  import { ref } from 'vue'
+  import { useProgressStore } from '@/stores/ProgressStore'
 
-  const steps = ref([
-    { name: 'Audience-Beginning', abbreviation: 'AB' },
-    { name: 'Introduction', abbreviation: 'I' },
-    { name: 'Main-1', abbreviation: '1' },
-    { name: 'Main-2', abbreviation: '2' },
-    { name: 'Main-3', abbreviation: '3' },
-    { name: 'End', abbreviation: 'E' },
-    { name: 'Audience-End', abbreviation: 'AE' }
-  ])
+  const store = useProgressStore()
+  const { steps } = store
 </script>
 
 <script>
--- src/stores/ProgressStore.js
+++ src/stores/ProgressStore.js
@@ -5,5 +5,15 @@
 import { defineStore } from 'pinia'
 
 export const useProgressStore = defineStore('ProgressStore', {
-  state: () => {}
+  state: () => ({
+    steps: [
+      { name: 'Audience-Beginning', abbreviation: 'AB' },
+      { name: 'Introduction', abbreviation: 'I' },
+      { name: 'Main-1', abbreviation: '1' },
+      { name: 'Main-2', abbreviation: '2' },
+      { name: 'Main-3', abbreviation: '3' },
+      { name: 'End', abbreviation: 'E' },
+      { name: 'Audience-End', abbreviation: 'AE' }
+    ]
+  })
 })
