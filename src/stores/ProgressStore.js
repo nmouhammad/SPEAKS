89400b9b1c6d900c91428aa4a5c267f6f9f39387
nmouhammad <39739945+nmouhammad@users.noreply.github.com>
2024-02-29 10:25:52 +0100

    Create ProgressStore
    (for now: boilerplate)

---
+++ src/stores/ProgressStore.js
@@ -0,0 +1,9 @@
+/**
+ *  This store saves the progress (i.e. the steps already done) for the message composition.
+ */
+
+import { defineStore } from 'pinia'
+
+export const useProgressStore = defineStore('ProgressStore', {
+  state: () => {}
+})
