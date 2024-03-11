636c3ccf4c76303571b2ba52048da87693ff01da
nmouhammad <39739945+nmouhammad@users.noreply.github.com>
2024-03-11 08:47:51 +0100

    Add SubstepsViewer
    The IntroductionView will consist of multiple substeps (i.e. multiple texts shown to the user after each other). However, all of them are part of preparing the introduction so we don't want to create separate views for all of the, especially since most are just texts + button. Therefore, IntroductionView will feed the SubstepsViewer the information for the current substep and then update the content if the substep changes.

--- src/views/IntroductionView.vue
+++ src/views/IntroductionView.vue
@@ -1,12 +1,18 @@
 <script>
+  import SubstepsViewer from '@/components/SubstepsViewer.vue'
+
   // @vuese
   // @group Views
   // This view guides the user through creating the introduction
   export default {
-    name: 'IntroductionView'
+    name: 'IntroductionView',
+    components: {
+      SubstepsViewer
+    }
   }
 </script>
 
 <template>
   <p>Introduction</p>
+  <SubstepsViewer text="example text" />
 </template>
---
+++ vuese-documentation/components/SubstepsViewer.md
@@ -0,0 +1,14 @@
+# SubstepsViewer
+
+If one view (e.g. Introduction) has different substeps, this component can be used to display one of the substeps at a time
+
+## Props
+
+<!-- @vuese:SubstepsViewer:props:start -->
+|Name|Description|Type|Required|Default|
+|---|---|---|---|---|
+|text|the text that should be displayed|`String`|`true`|-|
+
+<!-- @vuese:SubstepsViewer:props:end -->
+
+
