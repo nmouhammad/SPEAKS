d10148358f8782efa4a2c9cc06f664aca896005c
nmouhammad <39739945+nmouhammad@users.noreply.github.com>
2024-03-11 13:50:22 +0100

    Add texts to IntroductionView
    Before, we only had example texts, now IntroductionView already contains the first part of the actual texts.

--- src/views/IntroductionView.vue
+++ src/views/IntroductionView.vue
@@ -13,9 +13,19 @@
 </script>
 <script setup>
   const subtextArray = [
-    ['text 1.1', 'text 1.2', 'text 1.3'],
-    ['text 2.1', 'text 2.2', 'text 2.3'],
-    ['text 3.1', 'text 3.2', 'text 3.3']
+    [
+      'Great, now that we know more about your audience and what you want to reach with your presentation, we can start with preparing the actual presentation!'
+    ],
+    [
+      "First, we'll make sure to interest your audience into your topic.",
+      'A good chance for this is in your Introduction.',
+      'Many people waste this chance by starting with "Hello my name is HyTea and today we talk about xyz. This is my schedule. First, we start with ..."'
+    ],
+    [
+      'See how everyone is already falling asleep or looking at their phones?',
+      "Let's make sure this won't happen to you!",
+      "The good news is: there are multiple great options how to gain the audience's interest. Just pick what feels like the best match for you!"
+    ]
   ]
 </script>
 
