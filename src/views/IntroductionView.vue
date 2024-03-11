16cd61ea824ca1dbed324725a640c6c45e401895
nmouhammad <39739945+nmouhammad@users.noreply.github.com>
2024-03-11 10:27:57 +0100

    Enable switching between steps in SubstepsViewer
    We need to be able to switch between different steps in the SubstepViewer. For now, this functionality is enabled by providing buttons with triangles.

--- src/views/IntroductionView.vue
+++ src/views/IntroductionView.vue
@@ -11,8 +11,50 @@
     }
   }
 </script>
+<script setup>
+  import { ref } from 'vue'
+
+  const textArray = ['text 1', 'text 2', 'text 3']
+  const currentTextID = ref(0)
+
+  /**
+   * Increases the currentTextID if possible
+   */
+  function increaseTextID() {
+    if (currentTextID.value < textArray.length - 1) {
+      currentTextID.value++
+    }
+  }
+  /**
+   * Decreases the currentTextID if possible
+   */
+  function decreaseTextID() {
+    if (currentTextID.value > 0) {
+      currentTextID.value--
+    }
+  }
+
+  /**
+   * Checks whether there is a next step based on textArray
+   */
+  function nextIsPossible() {
+    return currentTextID.value < textArray.length - 1
+  }
+  /**
+   * Checks whether there is a previous step based on textArray
+   */
+  function previousIsPossible() {
+    return currentTextID.value > 0
+  }
+</script>
 
 <template>
   <p>Introduction</p>
-  <SubstepsViewer text="example text" />
+  <SubstepsViewer
+    :text="textArray[currentTextID]"
+    :next-is-possible="nextIsPossible()"
+    :previous-is-possible="previousIsPossible()"
+    @next="increaseTextID"
+    @previous="decreaseTextID"
+  />
 </template>
