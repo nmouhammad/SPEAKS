9e90300ab83a4b290548f51c23ac560cbd4dea76
nmouhammad <39739945+nmouhammad@users.noreply.github.com>
2024-03-15 08:01:15 +0100

    Remove arrow buttons from ChaptersViewer
    The arrow buttons were irritating (i.e. "When should I use these arrow buttons and when the 'Next' and 'Back' on the bottom?"). Additionally, they were only used in the IntroductionView and not in the other preparation steps. In the other preparation steps we provided only keyboard navigation. Therefore, the arrow buttons were removed to unify the different views.

--- src/components/ChaptersViewer.vue
+++ src/components/ChaptersViewer.vue
@@ -138,40 +138,6 @@
 <template>
   <!-- Changing the substep -->
   <KeyboardListener @next="next" @previous="previous" />
-  <div class="btn-group">
-    <!-- Chevron left -->
-    <button class="btn btn-outline-secondary" @click="previous">
-      <svg
-        xmlns="http://www.w3.org/2000/svg"
-        width="16"
-        height="16"
-        fill="currentColor"
-        class="bi bi-chevron-left"
-        viewBox="0 0 16 16"
-      >
-        <path
-          fill-rule="evenodd"
-          d="M11.354 1.646a.5.5 0 0 1 0 .708L5.707 8l5.647 5.646a.5.5 0 0 1-.708.708l-6-6a.5.5 0 0 1 0-.708l6-6a.5.5 0 0 1 .708 0"
-        />
-      </svg>
-    </button>
-    <!-- Chevron right -->
-    <button class="btn btn-outline-secondary" @click="next">
-      <svg
-        xmlns="http://www.w3.org/2000/svg"
-        width="16"
-        height="16"
-        fill="currentColor"
-        class="bi bi-chevron-right"
-        viewBox="0 0 16 16"
-      >
-        <path
-          fill-rule="evenodd"
-          d="M4.646 1.646a.5.5 0 0 1 .708 0l6 6a.5.5 0 0 1 0 .708l-6 6a.5.5 0 0 1-.708-.708L10.293 8 4.646 2.354a.5.5 0 0 1 0-.708"
-        />
-      </svg>
-    </button>
-  </div>
   <div class="container">
     <ChapterTextButton
       v-if="currentSubchapter.type === 'ChapterTextButton'"
