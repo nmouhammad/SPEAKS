44852335cbb09a5e16a5d2fbec1c144aac92d49f
nmouhammad <39739945+nmouhammad@users.noreply.github.com>
2024-03-28 13:08:19 +0100

    Adapt AudienceBeginningView to design v2
    All views need to use the new design. This commit updates AudienceBeginningView.vue to use the new design as well as to use the composition API instead of the Options API.

--- src/views/MC/AudienceBeginningView.vue
+++ src/views/MC/AudienceBeginningView.vue
@@ -1,35 +1,46 @@
-<script>
-  import ContentCollector from '@/components/ContentCollector.vue'
-  import CenteringContainer from '@/components/CenteringContainer.vue'
-  // @vuese
-  // @group Views
-  // This view contains all elements regarding the analysis of the audience in the beginning of the presentation
-  export default {
-    name: 'AudienceBeginningView',
-    components: { ContentCollector, CenteringContainer }
-  }
-</script>
-
 <script setup>
+  /**
+   * This view contains all elements regarding the analysis of the audience
+   * in the beginning of the presentation.
+   */
+  import ContentCollector from '@/components/ContentCollector.vue'
+  import ChatInterface from '@/components/ChatInterface.vue'
+  import TextBlocks from '@/components/TextBlocks.vue'
+  import UserInput from '@/components/UserInput.vue'
   import { storeToRefs } from 'pinia'
   import { useAudienceStore } from '@/stores/MC/AudienceStore'
 
   const store = useAudienceStore()
   const { knowledgeBeginning } = storeToRefs(store)
+
+  const texts1 = [
+    "Experts told us that it is really important for effective presentations to first consider the audience of the presentation. So that's what we will do now.",
+    'What does your audience already know about your topic?'
+  ]
+  const nrOfElementsInChapter = [2, 1]
 </script>
 
 <template>
-  <CenteringContainer>
-    <h5 class="text-center">
-      What does your audience already know about your topic?
-    </h5>
-    <ContentCollector
-      :all-content-to-display="knowledgeBeginning"
-      content-box-heading="Previous knowledge of your audience"
-      @add="store.addToknowledgeBeginning"
-      @remove="store.removeFromKnowledgeBeginning"
-    />
-  </CenteringContainer>
+  <ChatInterface :nr-of-elements-in-chapter="nrOfElementsInChapter">
+    <TextBlocks :chapter-i-d="0" :texts="texts1" />
+
+    <UserInput
+      :chapter-i-d="1"
+      heading="Collecting the audience's previous knowledge"
+      button-text="I'm done"
+    >
+      <div class="row justify-content-md-center">
+        <div class="col-6">
+          <ContentCollector
+            :all-content-to-display="knowledgeBeginning"
+            content-box-heading="Previous knowledge of your audience"
+            @add="store.addToknowledgeBeginning"
+            @remove="store.removeFromKnowledgeBeginning"
+          />
+        </div>
+      </div>
+    </UserInput>
+  </ChatInterface>
 </template>
 
 <style scoped></style>
