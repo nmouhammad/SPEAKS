946bce793e1fe8bb1b0a32024bf6bdf6dbf5d779
nmouhammad <39739945+nmouhammad@users.noreply.github.com>
2024-04-04 12:21:00 +0200

    Clearer variable names in ExampleView
    Since there are multiple textBlocks, it's clearer to put a number after every texts-array so that there is no confusion whether the array with the name "texts" (without number) is any different from the others.

--- src/views/MC/ExampleView.vue
+++ src/views/MC/ExampleView.vue
@@ -2,7 +2,7 @@
   import ChatInterface from '@/components/ChatInterface.vue'
   import TextBlocks from '@/components/TextBlocks.vue'
   import UserInput from '@/components/UserInput.vue'
-  const texts = ['t1', 't2']
+  const texts1 = ['t1', 't2']
   const texts2 = ['t 1.2', 't2.2']
 
   const nrOfElementsInChapter = [2, 1, 2]
@@ -10,7 +10,7 @@
 
 <template>
   <ChatInterface :nr-of-elements-in-chapter="nrOfElementsInChapter">
-    <TextBlocks :chapter-i-d="0" :texts="texts" />
+    <TextBlocks :chapter-i-d="0" :texts="texts1" />
     <UserInput :chapter-i-d="1" heading="heading" button-text="next">
       <p>Test</p>
     </UserInput>
