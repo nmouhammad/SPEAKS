cf8f675af80138e018f337b45f7876682ba8c3d6
nmouhammad <39739945+nmouhammad@users.noreply.github.com>
2024-04-04 11:37:26 +0200

    Update MiddleView to design v2
    This commit updates MiddleView.vue to use the new design.

--- src/views/MC/MiddleView.vue
+++ src/views/MC/MiddleView.vue
@@ -11,95 +11,87 @@
   import { ref } from 'vue'
   import { storeToRefs } from 'pinia'
 
-  import ChapterStepsHandler from '@/components/ChapterStepsHandler.vue'
-  import ThreePanes from '@/components/ThreePanes.vue'
   import ContentBox from '@/components/ContentBox.vue'
   import ContentCollector from '@/components/ContentCollector.vue'
+  import ChatInterface from '@/components/ChatInterface.vue'
+  import TextBlocks from '@/components/TextBlocks.vue'
+  import CenteringCol5 from '@/components/CenteringCol5.vue'
+  import UserInput from '@/components/UserInput.vue'
 
   import { useAudienceStore } from '@/stores/MC/AudienceStore'
   import { usePresentationPlanStore } from '@/stores/MC/PresentationPlanStore'
+
+  // +++++++++++++++++++++++
+  // ++++ Import stores ++++
+  // +++++++++++++++++++++++
 
   const audienceStore = useAudienceStore()
   const { knowledgeEnd, knowledgeBeginning } = storeToRefs(audienceStore)
   const presentationPlanStore = usePresentationPlanStore()
   const { middleContent } = storeToRefs(presentationPlanStore)
 
-  // ++++ Counters for which elements to show ++++
+  // ++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+  // ++++ Define content (e.g. text messages) that will be shown ++++
+  // ++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
 
-  // all elements in one chapter are shown after each other, when the next
-  // chapter begins, the content of the previous chapter is deleted
+  const texts1 = [
+    "We have the introduction to get the audience's interest and an end to summarize our main findings. Now it's time to create the middle part of the introduction.",
+    "For this, we'll look at what our audience knows before our presentation..."
+  ]
+  const texts2 = ['... and what they should know afterwards.']
+  const texts3 = ['How can we move them from the left to the right?']
 
-  // id of the last element in the current chapter that is shown
-  const currentElementID = ref(0)
-  const currentChapterID = ref(0)
-  // since not all chapters have the same length, we need to save the
-  // length of each chapter
-  const nrOfElementsInChapter = [5, 5]
+  // ++++++++++++++++++++++++++++++++++++++++++++++
+  // ++++ Additional variables & functionality ++++
+  // ++++++++++++++++++++++++++++++++++++++++++++++
 
-  /**
-   * Returns true if the element with the given elementID and chapterID should be visible
-   */
-  function isElementShown(elementID, chapterID) {
-    return (
-      currentChapterID.value == chapterID && elementID <= currentElementID.value
-    )
-  }
+  const nrOfElementsInChapter = [2, 1, 1, 1, 1, 1]
 </script>
 
 <template>
-  <ChapterStepsHandler
-    v-model:current-element-i-d="currentElementID"
-    v-model:current-chapter-i-d="currentChapterID"
-    :nr-of-elements-in-chapter="nrOfElementsInChapter"
-  />
-  <ThreePanes>
-    <template #left-pane>
-      <ContentBox
-        v-if="isElementShown(2, 0) || isElementShown(0, 1)"
-        :content-elements="knowledgeBeginning"
-        :has-remove-option="false"
-        content-box-heading="Your audience's knowledge before your presentation"
-      ></ContentBox>
-    </template>
-    <template #middle-pane>
-      <p v-if="isElementShown(0, 0)" class="text-center">
-        We have the introduction to get the audience's interest and an end to
-        summarize our main findings. Now it's time to create the middle part of
-        the introduction.
-      </p>
-      <p v-if="isElementShown(1, 0)" class="text-center">
-        For this, we'll look at what our audience knows before our
-        presentation...
-      </p>
-      <p v-if="isElementShown(3, 0)" class="text-center">
-        ... and what they should know afterwards.
-      </p>
-      <p
-        v-if="isElementShown(5, 0) || isElementShown(0, 1)"
-        class="text-center"
-      >
-        How can we move them from the left to the right?
-      </p>
-      <div
-        v-if="isElementShown(1, 1)"
-        class="d-flex flex-column align-items-center gap-3"
-      >
+  <ChatInterface :nr-of-elements-in-chapter="nrOfElementsInChapter">
+    <TextBlocks :chapter-i-d="0" :texts="texts1" />
+    <!-- Previous knowledge of the audience -->
+    <UserInput
+      :chapter-i-d="1"
+      heading="Recap your audience's previous knowledge"
+      button-text="I'm done"
+      ><CenteringCol5>
+        <ContentBox
+          :content-elements="knowledgeBeginning"
+          :has-remove-option="false"
+          content-box-heading="Your audience's knowledge before your presentation"
+        />
+      </CenteringCol5>
+    </UserInput>
+    <TextBlocks :chapter-i-d="2" :texts="texts2" />
+    <!-- Gained knowledge of the audience -->
+    <UserInput
+      :chapter-i-d="3"
+      heading="Recap your audience's gained knowledge"
+      button-text="I'm done"
+      ><CenteringCol5>
+        <ContentBox
+          :content-elements="knowledgeEnd"
+          :has-remove-option="false"
+          content-box-heading="Your audience's knowledge after your presentation"
+        />
+      </CenteringCol5>
+    </UserInput>
+    <TextBlocks :chapter-i-d="4" :texts="texts3" />
+    <!-- Content of your middle part -->
+    <UserInput
+      :chapter-i-d="5"
+      heading="Compose the content of your middle part"
+      button-text="I'm done"
+      ><CenteringCol5>
         <ContentCollector
-          class="gap-3"
           :all-content-to-display="middleContent"
           content-box-heading="Content of your middle part"
           @add="presentationPlanStore.addMiddleContent"
           @remove="presentationPlanStore.removeFromMiddleContent"
         />
-      </div>
-    </template>
-    <template #right-pane>
-      <ContentBox
-        v-if="isElementShown(4, 0) || isElementShown(0, 1)"
-        :content-elements="knowledgeEnd"
-        :has-remove-option="false"
-        content-box-heading="Your audience's knowledge after your presentation"
-      ></ContentBox>
-    </template>
-  </ThreePanes>
+      </CenteringCol5>
+    </UserInput>
+  </ChatInterface>
 </template>
