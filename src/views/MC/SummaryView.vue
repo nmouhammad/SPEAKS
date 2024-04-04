4096eea3e0481566ac14919bdad62e352d74244d
nmouhammad <39739945+nmouhammad@users.noreply.github.com>
2024-04-04 12:08:46 +0200

    Update SummaryView to design v2
    This commit updates SummaryView.vue to use the new design.

--- src/views/MC/SummaryView.vue
+++ src/views/MC/SummaryView.vue
@@ -2,37 +2,59 @@
   import { storeToRefs } from 'pinia'
   import ThreePanes from '@/components/ThreePanes.vue'
   import ContentBox from '@/components/ContentBox.vue'
+  import ChatInterface from '@/components/ChatInterface.vue'
+  import TextBlocks from '@/components/TextBlocks.vue'
+  import UserInput from '@/components/UserInput.vue'
   import { usePresentationPlanStore } from '@/stores/MC/PresentationPlanStore.js'
+
+  // +++++++++++++++++++++++
+  // ++++ Import stores ++++
+  // +++++++++++++++++++++++
 
   const PresentationPlanStore = usePresentationPlanStore()
   const { introductionContent, endContent, middleContent } = storeToRefs(
     PresentationPlanStore
   )
+
+  // ++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+  // ++++ Define content (e.g. text messages) that will be shown ++++
+  // ++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+
+  const texts1 = [
+    'Now you have created your full presentation plan. Good job! 💪',
+    'Here is an overview of your plan:'
+  ]
 </script>
 
 <template>
-  <h2 class="h2 mt-5 text-center">Your full presentation plan:</h2>
-  <ThreePanes>
-    <template #left-pane>
-      <ContentBox
-        :content-elements="introductionContent"
-        :has-remove-option="false"
-        content-box-heading="Introduction"
-      />
-    </template>
-    <template #middle-pane>
-      <ContentBox
-        :content-elements="middleContent"
-        :has-remove-option="false"
-        content-box-heading="Middle"
-      />
-    </template>
-    <template #right-pane>
-      <ContentBox
-        :content-elements="endContent"
-        :has-remove-option="false"
-        content-box-heading="End"
-      />
-    </template>
-  </ThreePanes>
+  <TextBlocks :chapter-i-d="0" :texts="texts1" />
+  <UserInput
+    :chapter-i-d="1"
+    heading="Overview of your full presentation plan:"
+    :custom-button="true"
+  >
+    <ThreePanes>
+      <template #left-pane>
+        <ContentBox
+          :content-elements="introductionContent"
+          :has-remove-option="false"
+          content-box-heading="Introduction"
+        />
+      </template>
+      <template #middle-pane>
+        <ContentBox
+          :content-elements="middleContent"
+          :has-remove-option="false"
+          content-box-heading="Middle"
+        />
+      </template>
+      <template #right-pane>
+        <ContentBox
+          :content-elements="endContent"
+          :has-remove-option="false"
+          content-box-heading="End"
+        />
+      </template>
+    </ThreePanes>
+  </UserInput>
 </template>
