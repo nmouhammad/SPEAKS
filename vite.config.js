a57437973140fddcc9f9dceb2ee91bbab578d0fd
nmouhammad <39739945+nmouhammad@users.noreply.github.com>
2024-04-23 13:08:45 +0200

    Test with gitlab-ci-file

--- vite.config.js
+++ vite.config.js
@@ -3,13 +3,22 @@ import { fileURLToPath, URL } from 'node:url'
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 
+function publicPath() {
+  if (process.env.CI_PAGES_URL) {
+    return new URL(process.env.CI_PAGES_URL).pathname
+  } else {
+    return '/'
+  }
+}
+
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: '/presentable-client-vue/',
   plugins: [vue()],
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url))
     }
-  }
+  },
+  outputDir: 'public',
+  publicPath: publicPath()
 })
