5922384d5b6357a6eeb0c05f3bc4007b22042f24
nmouhammad <39739945+nmouhammad@users.noreply.github.com>
2024-04-25 09:52:57 +0200

    Change ProgressBar circles to arrows
    To better visually illustrate that the steps are steps that follow each other and to visually separate them from "normal" buttons, the progress bar icons are now arrows instead of circles.
    There are three types of arrows: before, middle and after. For each arrow type there is a outline and a filled version as well as a template (created in inkscape) which can be used to make changes.

---
+++ src/components/ProgressBarArrow.vue
@@ -0,0 +1,187 @@
+<script>
+  // @vuese
+  // @group Components
+  // One of the circles representing one step in the ProgressBar.
+  export default {
+    name: 'ProgressBarCircle',
+    props: {
+      // Abbreviation of the step title (should be <=2 letters) that will be displayed inside the circle
+      abbreviation: {
+        type: String,
+        required: true
+      },
+      // True, if this step is the current step, false otherwise
+      isCurrent: {
+        type: Boolean,
+        default: false
+      },
+      // True if this step should already be visible
+      isShown: {
+        type: Boolean,
+        default: false
+      }
+    }
+  }
+</script>
+
+<script setup>
+  import { useProgressStore } from '@/stores/MC/ProgressStore'
+  const store = useProgressStore()
+
+  /**
+   * Converts the abbreviation of a step into the arrow type
+   * (arrow types are: "Before", "After" and "Middle")
+   * @param {String} abbr abbreviation of the current step
+   */
+  function abbreviationToArrowType(abbr) {
+    return abbr == 'Before' || abbr == 'After' ? abbr : 'Middle'
+  }
+
+  /**
+   * To convert the arrow type + whether it is the current step
+   * into the correct css class for the arrow (= right svg file)
+   */
+  const situationToCssClass = {
+    Before: {
+      true: 'svg-button-before',
+      false: 'svg-button-before-outline'
+    },
+    Middle: {
+      true: 'svg-button-middle',
+      false: 'svg-button-middle-outline'
+    },
+    After: {
+      true: 'svg-button-after',
+      false: 'svg-button-after-outline'
+    }
+  }
+</script>
+
+<template>
+  <!-- <button
+    v-show="isShown"
+    class="btn arrow"
+    :class="[isCurrent ? 'btn-primary' : 'btn-outline-primary']"
+    @click="store.changeStep(abbreviation)"
+  >
+    {{ abbreviation }}
+  </button> -->
+  <button
+    v-show="isShown"
+    class="svg-button"
+    :class="
+      situationToCssClass[abbreviationToArrowType(abbreviation)][isCurrent]
+    "
+    @click="store.changeStep(abbreviation)"
+  >
+    {{ abbreviation }}
+  </button>
+</template>
+
+<style scoped>
+  .svg-button-before {
+    background-image: url('@/assets/arrow-before.svg');
+    width: 144px; /* Set the width of the button */
+  }
+  .svg-button-before-outline {
+    background-image: url('@/assets/arrow-before-outline.svg');
+    width: 144px; /* Set the width of the button */
+  }
+  .svg-button-middle {
+    background-image: url('@/assets/arrow-middle.svg');
+    width: 140px; /* Set the width of the button */
+  }
+  .svg-button-middle-outline {
+    background-image: url('@/assets/arrow-middle-outline.svg');
+    width: 140px; /* Set the width of the button */
+  }
+  .svg-button-after {
+    background-image: url('@/assets/arrow-after.svg');
+    width: 121px; /* Set the width of the button */
+  }
+  .svg-button-after-outline {
+    background-image: url('@/assets/arrow-after-outline.svg');
+    width: 121px; /* Set the width of the button */
+  }
+  .svg-button {
+    background-size: cover;
+    background-color: transparent;
+    background-repeat: no-repeat; /* Prevent repeating of the SVG */
+    background-position: center; /* Center the SVG within the button */
+    background-color: transparent;
+    height: 50px; /* Set the height of the button */
+    border: none;
+    cursor: pointer;
+  }
+  /* modified from https://bootsnipp.com/snippets/dKB */
+  .btn-lg {
+    text-align: center;
+    width: 80px;
+    height: 50px;
+    padding: 10px 5px;
+    font-size: 18px;
+    line-height: 1.33;
+    border-radius: 25px;
+  }
+
+  /* based on https://stackoverflow.com/a/70797508 */
+
+  .arr {
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    height: 50px;
+    width: 150px;
+    position: relative;
+    background: rgba(var(--bs-light-rgb));
+    clip-path: polygon(75% 0%, 100% 50%, 75% 100%, 0% 100%, 15% 50%, 0% 0%);
+  }
+
+  .arr-non-active:before {
+    content: '';
+    background: white;
+    height: 44px;
+    width: 138px;
+    position: absolute;
+    left: 50%;
+    top: 50%;
+    transform: translate(-50%, -50%);
+    clip-path: polygon(75% 0%, 100% 50%, 75% 100%, 0% 100%, 15% 50%, 0% 0%);
+  }
+  .arrow {
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    height: 50px;
+    width: 150px;
+    position: relative;
+    clip-path: polygon(75% 0%, 100% 50%, 75% 100%, 0% 100%, 15% 50%, 0% 0%);
+  }
+
+  .arrow-outline {
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    height: 50px;
+    width: 150px;
+    position: relative;
+    background: black;
+    clip-path: polygon(75% 0%, 100% 50%, 75% 100%, 0% 100%, 15% 50%, 0% 0%);
+  }
+
+  .arrow-outline:after {
+    content: '';
+    background: white;
+    height: calc(100% - 2px);
+    width: calc(100% - 3px);
+    position: absolute;
+    left: 50%;
+    top: 50%;
+    transform: translate(-50%, -50%);
+    clip-path: polygon(75% 0%, 100% 50%, 75% 100%, 0% 100%, 15% 50%, 0% 0%);
+  }
+  div span {
+    z-index: 2;
+    position: relative;
+  }
+</style>
